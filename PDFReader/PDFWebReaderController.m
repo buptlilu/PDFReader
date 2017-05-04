503dffc1a643364661fae3b1e87682ea6e4992c7
lilu <chujunhe1234@163.com>
2017-05-04 16:35:37 +0800

    change UIMenuControl items

--- PDFReader/PDFWebReaderController.m
+++ PDFReader/PDFWebReaderController.m
@@ -15,6 +15,20 @@
 @implementation PDFWebReaderController
 
 #pragma mark - life cycle
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    UIMenuItem *customMenuItem1 = [[UIMenuItem alloc] initWithTitle:@"Custom 1" action:@selector(customAction1:)];
+    UIMenuItem *customMenuItem2 = [[UIMenuItem alloc] initWithTitle:@"Custom 2" action:@selector(customAction2:)];
+    [[UIMenuController sharedMenuController] setMenuItems:[NSArray arrayWithObjects:customMenuItem1, customMenuItem2, nil]];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    
+    [[UIMenuController sharedMenuController] setMenuItems:nil];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
@@ -32,5 +46,31 @@
     
     // 通过url加载文件
     [self.webView loadRequest:request];
+}
+
+#pragma mark - functions
+- (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
+    if (action == @selector(_define:)) {
+        return NO;
+    }
+    if (action == @selector(selectAll:)) {
+        return YES;
+    }
+    
+    if ([self.webView isFirstResponder]) {
+        if (action == @selector(customAction1:) || action == @selector(customAction2:)) {
+            return YES;
+        }
+    }
+    
+    return [super canPerformAction:action withSender:sender];
+}
+
+- (void)customAction1:(UIMenuItem *)item {
+    NSLog(@"customAction1");
+}
+
+- (void)customAction2:(UIMenuItem *)item {
+    NSLog(@"customAction2");
 }
 @end
